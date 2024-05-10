c5c71c3dc1f7cf0a6532d7326c427d3a17140085
ahmedh3lal <92175577+ahmedh3lal@users.noreply.github.com>
2024-05-10 14:16:07 +0300

    Add files via upload

---
+++ tech/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace tech.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
